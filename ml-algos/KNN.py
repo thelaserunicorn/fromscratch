4347af6c5598db3a9d08c7f3a248c2ea3efbd06f
thelaserunicorn <rashideram2@gmail.com>
2020-11-04 17:01:05 +0600

    KNN

--- ml-algos/KNN.py
+++ ml-algos/KNN.py
@@ -1,1 +1,27 @@
-def KNN:
+import numpy as np
+from collections import counter
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum(x1-x2)**2)
+
+class KNN:
+
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        predicted_labels = [self._predict(x) for x in X]
+        return np.array(predicted_labels)
+
+    def _predict(self, x):
+        #Euclidean Distance
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        knn_labels = [self.y_train[i] for i in k_indices]
+        majority_vote = Counter(knn_labels).majority_vote(1)
+        return majority_vote[0][0]
+
