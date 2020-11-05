6cd89f5c385558b31732a2afa244da3f561e3f9a
thelaserunicorn <rashideram2@gmail.com>
2020-11-05 16:10:36 +0600

    linear regression

---
+++ ml-algos/linear_regression.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class LinearRegression:
+    def _init__(self, lr=0.001, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = weights
+        self.bias = none
+
+    def fit(self, X, y):
+
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters)
+        y_predicted = np.dot(X, self.weights) + bias
+
+        dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
+        db = (1/n_samples) * np.sum(y_predicted - y)
+
+        self.weights -= self.lr * dw
+        self.bias -= self.lr * db
+
+
+    def predict(self, X):
+
+        y_predicted = np.dot(X, self.weights) + bias
+        return y_predicted
