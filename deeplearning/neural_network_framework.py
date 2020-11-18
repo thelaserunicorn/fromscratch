dc2db9a56ac40cb4ad474642bc655bea03cd1f44
thelaserunicorn <rashideram2@gmail.com>
2020-11-18 14:53:34 +0600

    added forward pass

--- deeplearning/neural_network_framework.py
+++ deeplearning/neural_network_framework.py
@@ -1,2 +1,22 @@
-class Dense:
-    pass
+import numpy as np
+import nnfs
+from nnfs.datasets import sine_data, spiral_data
+nnfs.init()
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons,
+                 weight_regularizer_l1=0, weight_regularizer_l2=0,
+                 bias_regularizer_l1=0, bias_regularizer_l2=0):
+        # Initializing weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros(1, n_neurons)
+        # Regularization strength
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
+
+    def forward(self, inputs, training):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
