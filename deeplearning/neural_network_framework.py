1da7c4a85e0d150e9d1ea63e0cbdea3cb278b284
thelaserunicorn <rashideram2@gmail.com>
2020-11-18 14:55:39 +0600

    bias fix

--- deeplearning/neural_network_framework.py
+++ deeplearning/neural_network_framework.py
@@ -10,7 +10,7 @@ class Layer_Dense:
                  bias_regularizer_l1=0, bias_regularizer_l2=0):
         # Initializing weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros(1, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
         # Regularization strength
         self.weight_regularizer_l1 = weight_regularizer_l1
         self.weight_regularizer_l2 = weight_regularizer_l2
